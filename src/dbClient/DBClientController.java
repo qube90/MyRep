f5d38b8131aef07441c956cf7c31fa1320fcca04
qube90 <jakub.k.krolak@gmail.com>
2018-02-14 14:14:49 +0100

    TODO: getting data from sql file

--- src/dbClient/DBClientController.java
+++ src/dbClient/DBClientController.java
@@ -2,15 +2,18 @@ package dbClient;
 
 import dbUtil.dbConnection;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class DBClientController implements Initializable {
@@ -32,7 +35,7 @@ public class DBClientController implements Initializable {
     private ChoiceBox<ProductType> choiceBox;
 
 
-
+    public String table;
 
     private dbConnection dc;
 
@@ -42,6 +45,45 @@ public class DBClientController implements Initializable {
         this.choiceBox.setItems(FXCollections.observableArrayList(ProductType.values()));
 
     }
+
+    public void loadData(int offset) {
+        try {
+            Connection con = dbConnection.getConnection();
+            if (choiceBox.getValue().toString() == "Kwiaty") {
+                 table = "kwiaty";
+             } else if (choiceBox.getValue().toString() == "Krzewy"){
+                table = "krzewy";
+            }else if (choiceBox.getValue().toString() == "Drzewa"){
+                table = "drzewa";
+            }else if (choiceBox.getValue().toString() == "Narzędzia"){
+                table = "narzedzia";
+            }else if (choiceBox.getValue().toString() == "Nawozy"){
+                table = "nawozy";
+            }else if (choiceBox.getValue().toString() == "Inne"){
+                table = "inne";
+            } else {
+                table = null;
+            }
+
+            String sqlLine = "SELECT * FROM " + table + " LIMIT 1 OFFSET " + Integer.toString(offset);
+
+            Image image = new Image("D:\\programowanie\\pliki programów nauka\\Kamil1\\probny_kwiaty.jpg");
+
+            ResultSet rs = con.createStatement().executeQuery(sqlLine);
+
+            this.labelName.setText(rs.getString(1));
+            this.labelPrice.setText(rs.getString(2));
+            this.labelDescription.setText(rs.getString(3));
+            this.imageView.setImage(image);
+
+
+        } catch (SQLException ex){
+            ex.printStackTrace();
+        }
+    }
+
+
+
 
 
 
